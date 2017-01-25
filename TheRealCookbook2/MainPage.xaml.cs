b7348a10b5e4f0645cc4afd3f8a8b0deb314c030
Joshua Hood <xjhood81x@gmail.com>
2017-01-25 11:32:56 -0500

    This is project!

---
+++ TheRealCookbook2/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net.Http;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TheRealCookbook
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class MainPage : Page
+    {
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        //Get our api key 
+        public static string KGetter()
+        {
+            var reasources = new Windows.ApplicationModel.Resources.ResourceLoader("Resources");
+            string key = reasources.GetString("Key1");
+
+            return key;
+        }
+        //this is the get Recipes button click event----- gets url and title--- serchproxy
+        private async void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            SearchProxyClass.ProxyClass.RootObject SearchObject = await SearchProxyClass.SearchObject(txtbox1.Text);
+            hyperlinkRecipeButton.NavigateUri = new Uri(SearchObject.recipes[0].source_url);
+            hyperlinkRecipeButton.Content = SearchObject.recipes[0].title;
+
+            //set image source to populate on application
+            string path = SearchObject.recipes[0].image_url;
+            BitmapImage bitmap = new BitmapImage(new Uri(path));
+            UrlPicture.Source = bitmap;
+            // try to get another to populate
+            hyperlinkRecipeButton1.NavigateUri = new Uri(SearchObject.recipes[1].source_url);
+            hyperlinkRecipeButton1.Content = SearchObject.recipes[1].title;
+            //second picture
+            string path1 = SearchObject.recipes[1].image_url;
+            BitmapImage bitmap1 = new BitmapImage(new Uri(path1));
+            UrlPicture1.Source = bitmap1;
+
+        }
+
+
+        //trying to make hyperlink
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void onRecipeButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                Uri uri = new Uri(hyperlinkRecipeButton.NavigateUri.ToString());
+                myweb.Navigate(uri);
+            }
+            catch
+            {
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ TheRealCookbook2/SearchProxyClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheRealCookbook
+{
+    class SearchProxyClass
+    {
+        public async static Task<ProxyClass.RootObject> SearchObject(string search1)
+        {
+            var key = MainPage.KGetter();
+            var http = new HttpClient();
+            var response = await http.GetAsync(String.Format("http://food2fork.com/api/search?key={0}&q={1}", key, search1));
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(ProxyClass.RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (ProxyClass.RootObject)serializer.ReadObject(ms);
+            return data;
+
+
+
+        }
+        public class ProxyClass
+        {
+            [DataContract]
+            public class Recipe
+            {
+                [DataMember]
+                public string publisher { get; set; }
+                [DataMember]
+                public string f2f_url { get; set; }
+                [DataMember]
+                public string title { get; set; }
+                [DataMember]
+                public string source_url { get; set; }
+                [DataMember]
+                public string recipe_id { get; set; }
+                [DataMember]
+                public string image_url { get; set; }
+                [DataMember]
+                public double social_rank { get; set; }
+                [DataMember]
+                public string publisher_url { get; set; }
+            }
+            [DataContract]
+            public class RootObject
+            {
+                [DataMember]
+                public int count { get; set; }
+                [DataMember]
+                public List<Recipe> recipes { get; set; }
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
